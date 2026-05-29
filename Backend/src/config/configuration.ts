107de12fdaea22860f1e501313742acaebc5a638
tinbao <tinbao567@example.com>
2026-05-29 11:41:18 +0700

    add

--- Backend/src/config/configuration.ts
+++ Backend/src/config/configuration.ts
@@ -7,5 +7,5 @@ type AppConfig = {
 export default (): AppConfig => ({
   env: process.env.NODE_ENV ?? 'development',
   port: Number(process.env.PORT ?? 3001),
-  mongoUri: process.env.MONGO_URI ?? 'mongodb://localhost:27017/reveal-quiz-race',
+  mongoUri: process.env.MONGO_URI ?? 'mongodb+srv://tinbao567_db_user:rMGuH69lL6oWc9iB@cluster0.1ynenbi.mongodb.net/',
 });

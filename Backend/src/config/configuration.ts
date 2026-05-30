7da8af712f7f42644fd649cf3c6d909dfe533bfa
tinbao <tinbao567@example.com>
2026-05-30 10:20:56 +0700

    chore: update mongodb connection config

--- Backend/src/config/configuration.ts
+++ Backend/src/config/configuration.ts
@@ -7,5 +7,5 @@ type AppConfig = {
 export default (): AppConfig => ({
   env: process.env.NODE_ENV ?? 'development',
   port: Number(process.env.PORT ?? 3001),
-  mongoUri: process.env.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/reveal_quiz_race',
+  mongoUri: process.env.MONGO_URI ?? 'mongodb+srv://tinbao567_db_user:rMGuH69lL6oWc9iB@cluster0.1ynenbi.mongodb.net/',
 });
